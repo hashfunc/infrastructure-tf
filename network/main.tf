7c9d3d8504f1366dfd695398db78b9071ce02cd8
hashfunc <root@hashfunc.io>
2024-12-16 20:47:14 +0900

    feat(network): Add database subnets

--- network/main.tf
+++ network/main.tf
@@ -33,6 +33,14 @@ locals {
           [for _ in local.vpc.azs : 4]...
         )
       }
+    },
+    database = {
+      main = {
+        cidrs = cidrsubnets(
+          cidrsubnet(local.vpc.cidr, 4, 3),
+          [for _ in local.vpc.azs : 4]...
+        )
+      }
     }
   }
 }
@@ -76,10 +84,25 @@ module "vpc" {
     ]
   ])
 
+  database_subnets = flatten([
+    for subnet in local.subnets.database : subnet.cidrs
+  ])
+  database_subnet_names = flatten([
+    for name, subnet in local.subnets.database : [
+      for idx in range(length(subnet.cidrs)) :
+      "${local.vpc.name}-database-${name}-${idx}"
+    ]
+  ])
+
+  create_database_subnet_group           = true
+  create_database_subnet_route_table     = true
+  create_database_internet_gateway_route = true
+
   manage_default_route_table    = false
   manage_default_network_acl    = false
   manage_default_security_group = false
 
   enable_nat_gateway = true
   single_nat_gateway = var.vpc.single_nat_gateway
+
 }
